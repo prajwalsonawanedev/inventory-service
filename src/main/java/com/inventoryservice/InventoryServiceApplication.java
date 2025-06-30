2456025171bd0388780986deac999c14647d856e
prajwal-sonawane <prajwalsonawane.dev@gmail.com>
2025-06-30 18:25:13 +0530

    initial commit

---
+++ src/main/java/com/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+        System.out.println("Inventory Service !");
+    }
+
+}
